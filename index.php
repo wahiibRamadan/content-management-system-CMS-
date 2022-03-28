69e3ff7fa7f0f027e62cb9e87367a0a7f387cc78
Wahiib Ramadan <92620871+wahiibRamadan@users.noreply.github.com>
2022-03-28 17:44:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-    <!-- <form method="post" action="display.php">
+    <form method="post" action="display.php">
     <label >userName</label>
     <input type="text" name="name">
     <input type="submit" name="submit">
-    </form> -->
+    </form>
 
 
 <?php
